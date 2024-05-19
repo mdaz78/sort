b0a36569821a1d2bab04ee12841282db2eeeafbc
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-19 18:12:19 +0530

    chore: Add sorting functionality for CharactersCollection and refactor Sorter class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+import { CharactersCollection } from "./CharacterCollections";
 import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
 const numbersCollection = new NumbersCollection([10, 0, 3, -5, 0]);
 const numberSorter = new Sorter(numbersCollection);
 
+const charactersCollection = new CharactersCollection("Xaayb");
+const characterSorter = new Sorter(charactersCollection);
+
 numberSorter.sort();
+characterSorter.sort();
 
 console.log(numbersCollection.data);
+console.log(charactersCollection.data);
