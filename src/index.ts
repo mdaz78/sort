edf92fb50c2f21054b577fb62944e607da798bd6
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-19 18:05:05 +0530

    refactor: update numbers in NumbersCollection and rename sorter variable

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
-const numbersCollection = new NumbersCollection([100, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+const numbersCollection = new NumbersCollection([10, 0, 3, -5, 0]);
+const numberSorter = new Sorter(numbersCollection);
 
-sorter.sort();
+numberSorter.sort();
 
 console.log(numbersCollection.data);
